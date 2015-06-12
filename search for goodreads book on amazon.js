ae0cec029cc9018486307a23a6175b6c9b924dc4
wyldphyre <craigreynolds@Claire.local>
2015-06-12 22:15:08 +0800

    Include first author in the Amazon search.

--- search for goodreads book on amazon.js
+++ search for goodreads book on amazon.js
@@ -6,6 +6,7 @@
 javascript:(function(){
     // todo: alert if not on a goodreads book pag
     var bookTitle = document.getElementById("bookTitle").firstChild.data;
-    // todo: located and include the author
-    window.open("http://www.amazon.com.au/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=" + bookTitle.trim().replace(/\s/g, "+"));
-})()
+    var firstAuthor = document.getElementsByClassName("authorName")[0].firstChild.innerHTML;
+    var titleAndFirstAuthor = (bookTitle.trim() + " " + firstAuthor).replace(/\s/g, "+");
+
+    window.open("http://www.amazon.com.au/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=" + titleAndFirstAuthor);})()
