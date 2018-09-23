69707bbf5bbe415457fdb0acf17fbcde6155c4ea
Craig Reynolds <wyldphyre@gmail.com>
2018-09-23 09:07:25 +0800

    Functional and can be used in a Shortcuts javascript action to return a url to open.

--- Find Goodreads book on Amazon.js
+++ Find Goodreads book on Amazon.js
@@ -1,7 +1,10 @@
-function(){
-  var bookTitle = document.getElementById("bookTitle").firstChild.data;
-  var firstAuthor = document.getElementsByClassName("authorName")[0].firstChild.innerHTML;
-  var titleAndFirstAuthor = (bookTitle.trim() + " " + firstAuthor).replace(/\s/g, "+");
+
+let bookTitle = document.getElementById("bookTitle").firstChild.data;
+let firstAuthor = document.getElementsByClassName("authorName")[0].firstChild.innerHTML;
+let titleAndFirstAuthor = (bookTitle.trim() + " " + firstAuthor).replace(/\s/g, "+");
   
-  window.open("http://www.amazon.com.au/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=" + titleAndFirstAuthor);
-}
+let searchUrl = "http://www.amazon.com.au/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=" + titleAndFirstAuthor;
+
+// Call completion to finish
+let result = [searchUrl];
+completion(result);
